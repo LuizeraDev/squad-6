8c6bd260fe78f2ee52f7520394228f73cff35996
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-01 16:15:37 -0300

    alteracoes sala santos

--- salas/criar-sala-santos.php
+++ salas/criar-sala-santos.php
@@ -6,7 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Criar Sala - Santos</title>
 </head>
+<style>
+    .container{
+        margin-top: 15%;
+    }
+</style>
 <body>
-    
+    <main>
+        <!-- No php, pegamos dados através do campo "name" dos inputs -->
+        <section class="container" align="center">
+            <form action="funcionalidades/inserir.php" method="POST">
+            <h3>Exemplo básico de aplicação</h3>
+            <input type="text" name="nome">
+            <p>
+            Santos
+            <input type="radio" name="cidade" value="Santos">
+            São Paulo
+            <input type="radio" name="cidade" value="SaoPaulo">
+            <br><br>
+            <input type="submit" value="Entrar">
+            </p>
+            </form>
+        </section>
+    </main>
 </body>
 </html>
