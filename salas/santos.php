247b002c65836f1c4790a9fddfc0628440e6c72f
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-02 02:36:26 -0300

    exibindo salas/nomes/imagens

--- salas/santos.php
+++ salas/santos.php
@@ -1,6 +1,14 @@
 <?php 
+    require("../funcionalidades/conecta-banco.php");
     session_start();
     $_SESSION["santos"] = true;
+
+    // seleciona código da sala criada
+    $comandoSQL = "SELECT * from tb_sala_santos WHERE cd_sala_santos IS NOT NULL";
+    $resultado_sala = mysqli_query($con, $comandoSQL) or die("Erro no banco de dados!");
+    $dados = mysqli_fetch_all($resultado_sala, MYSQLI_ASSOC);
+    // pega o número de linhas no banco de dados que condizem com a condição do select
+    $quantidade = mysqli_num_rows($resultado_sala);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -14,6 +22,16 @@
     <h1>LOGOTIPO - FIFO</h1>
     <h4>Criar nova sala? <a href="criar-sala-santos.php">Clique aqui</a></h4>
     <h3>Selecione seu jogo</h3>
+    <?php 
+        if($quantidade != null){
+            for($i = 0; $i < $quantidade; $i++){
+                echo "<p>Nome da sala: ".$dados[$i]['nm_sala'] ."</p>";
+                echo "<p>Imagem da sala: </p>";
+                echo "<img src='../funcionalidades/img-salas-santos/".$dados[$i]['cd_sala_santos']."/".$dados[$i]['img_sala']."'>";
+                echo "<br>";
+            }
+        }   
+    ?>
     <a href="nome.php">Selecionar jogo</a>
     <br><br>
     <a href="../index.php">Voltar ao início</a>
--- salas/saopaulo.php
+++ salas/saopaulo.php
@@ -1,6 +1,14 @@
 <?php 
+    require("../funcionalidades/conecta-banco.php");
     session_start();
     $_SESSION["saopaulo"] = true;
+
+     // seleciona código da sala criada
+     $comandoSQL = "SELECT * from tb_sala_sao_paulo WHERE cd_sala_sao_paulo IS NOT NULL";
+     $resultado_sala = mysqli_query($con, $comandoSQL) or die("Erro no banco de dados!");
+     $dados = mysqli_fetch_all($resultado_sala, MYSQLI_ASSOC);
+     // pega o número de linhas no banco de dados que condizem com a condição do select
+     $quantidade = mysqli_num_rows($resultado_sala);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -14,6 +22,16 @@
     <h1>LOGOTIPO - FIFO</h1>
     <h4>Criar nova sala? <a href="criar-sala-sao-paulo.php">Clique aqui</a></h4>
     <h3>Selecione seu jogo</h3>
+    <?php 
+        if($quantidade != null){
+            for($i = 0; $i < $quantidade; $i++){
+                echo "<p>Nome da sala: ".$dados[$i]['nm_sala'] ."</p>";
+                echo "<p>Imagem da sala: </p>";
+                echo "<img src='../funcionalidades/img-salas-sao-paulo/".$dados[$i]['cd_sala_sao_paulo']."/".$dados[$i]['img_sala']."'>";
+                echo "<br>";
+            }
+        }   
+    ?>
     <a href="nome.php">Selecionar jogo</a>
     <br><br>
     <a href="../index.php">Voltar ao início</a>
