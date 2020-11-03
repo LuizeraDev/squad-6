9a53a791590fe6210a3113bb503bd58f8d91753d
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-03 18:33:51 -0300

    Consertando bug de exebição na ordem da fila e limitando exibição para 5 pessoas :rocket:

--- funcionalidades/fila-assincrona.php
+++ funcionalidades/fila-assincrona.php
@@ -5,19 +5,21 @@ error_reporting(0);
 
 if (isset($_SESSION["santos"])) {
     // Seleciona todos os usuários que tem o mesmo código de sala
-    $comandoSQL = "SELECT * FROM tb_usuario WHERE cd_sala_santos = '$codigo_sala'";
+    $comandoSQL = "SELECT * FROM tb_usuario WHERE cd_sala_santos = '$codigo_sala' ORDER BY cd_fila_usuario";
     $result_users = mysqli_query($con, $comandoSQL);
     $usuarios = mysqli_fetch_all($result_users, MYSQLI_ASSOC);
     $qt_linhas = mysqli_num_rows($result_users);
 } else {
     // Seleciona todos os usuários que tem o mesmo código de sala
-    $comandoSQL = "SELECT * FROM tb_usuario WHERE cd_sala_sao_paulo = '$codigo_sala'";
+    $comandoSQL = "SELECT * FROM tb_usuario WHERE cd_sala_sao_paulo = '$codigo_sala' ORDER BY cd_fila_usuario";
     $result_users = mysqli_query($con, $comandoSQL);
     $usuarios = mysqli_fetch_all($result_users, MYSQLI_ASSOC);
     $qt_linhas = mysqli_num_rows($result_users);
 }
 
 for ($i = 0; $i < $qt_linhas; $i++) {
-    echo "<b>Posição na fila: ".$usuarios[$i]['cd_fila_usuario']." | Nome: ".$usuarios[$i]['nm_usuario']."</b>";
-    echo "<br><br>";
+    if($qt_linhas < 4){
+        echo "<b>Posição na fila: ".$usuarios[$i]['cd_fila_usuario']." | Nome: ".$usuarios[$i]['nm_usuario']."</b>";
+        echo "<br><br>";
+    }
 } 
