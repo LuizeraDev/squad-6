7cd72adf40696362df7d5a5a50fbab15e2ed3849
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-04 09:56:28 -0300

    Adicionando exclusão de sala por número gerado aleatoriamente

--- salas/santos.php
+++ salas/santos.php
@@ -28,11 +28,34 @@ $quantidade = mysqli_num_rows($resultado_sala);
                 echo "<p>Nome da sala: ". $dados[$i]['nm_sala'] ."</p>";
                 echo "<p>Imagem da sala: </p>";
                 echo "<img src='../funcionalidades/img-salas-santos/".$dados[$i]['nm_sala']."/".$dados[$i]['img_sala']."'>";
-                echo "<br><a href='fila.php?id=". $dados[$i]['cd_sala_santos'] ."'>Selecionar sala</a>";
+                echo "<br><a href='fila.php?id=". $dados[$i]['cd_sala_santos'] ."'>Selecionar sala</a><br><br>";
+                echo "<p>Digite <span id='aleatorio'></span> para excluir a sala</p>";
+                echo "<input type='text' id='numero_aleatorio' maxlength='3'><br><br>";
+                echo "<input type='submit' onclick='excluir()' value='Apagar Sala'>";
+                echo "<p id='resposta'></p>";
                 echo "<br><br>";
+                echo "
+                <script>
+                    var aleatorio = Math.floor(Math.random() * (999 - 100) + 100);
+                    var exibir = document.all['aleatorio'];
+                    exibir.innerHTML = aleatorio;
+                    function excluir ()  
+                    {
+                        var valor_digitado = document.all('numero_aleatorio').value;
+                        var resposta = document.all['resposta'];
+        
+                        if (valor_digitado  == aleatorio) {
+                            window.location.href='../funcionalidades/excluir-sala.php?id=". $dados[$i]['cd_sala_santos'] ."';
+                        } else {
+                            resposta.innerHTML = 'O valor informado não confere';
+                        }
+                    }   
+                </script>
+                ";
             }
         }   
     ?>
-    <a href="../index.php">Voltar ao início</a>
+    <a href="unidade.php">Voltar a escolha de unidade</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="../index.php">Voltar ao login</a>
 </body>
 </html>
