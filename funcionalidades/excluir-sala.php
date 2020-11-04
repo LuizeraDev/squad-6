d4b6a553d70600bc7537d23d844af584595aaf32
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-04 10:26:16 -0300

    Refeito o código de exclusão de salas :fire:

--- funcionalidades/excluir-sala.php
+++ funcionalidades/excluir-sala.php
@@ -2,63 +2,39 @@
 require("../funcionalidades/conecta-banco.php");
 session_start();
 
-$senha_sala = $_POST['senha-sala'];
 $codigo_sala = $_GET['id'];
 
+
 if (isset($_SESSION["santos"])) {
-    // Busca a senha da sala no banco de dados
-    $comandoSQL = "SELECT cd_senha_sala from tb_sala_santos WHERE cd_sala_santos='$codigo_sala'";
-    $resultado_sala = mysqli_query($con, $comandoSQL) or die("Erro no banco de dados!");
-    $senha = mysqli_fetch_array($resultado_sala);
-
-    if ($senha_sala === $senha[0]) {
         // Pega o nome da imagem no banco de dados
-        $comandoSQL = "SELECT img_sala from tb_sala_santos WHERE cd_sala_santos='$codigo_sala'";
-        $resultado_sala = mysqli_query($con, $comandoSQL) or die("Erro no banco de dados!");
-        $nome_img = mysqli_fetch_array($resultado_sala);
-
-        // Deleta os usuários que possuem a chave estrangeira da sala
-        $comandoSQL = "DELETE FROM tb_usuario WHERE cd_sala_santos='$codigo_sala'";
-        $con->query($comandoSQL) or die("algo deu errado");
+        $comandoSQL = "SELECT nm_sala, img_sala from tb_sala_santos WHERE cd_sala_santos='$codigo_sala'";
+        $resultado_sala = mysqli_query($con, $comandoSQL);
+        $sala = mysqli_fetch_array($resultado_sala);
 
         // Deleta a sala criada no banco de dados
         $comandoSQL = "DELETE FROM tb_sala_santos WHERE cd_sala_santos='$codigo_sala'";
         $con->query($comandoSQL) or die("algo deu errado");
+        $con->close();
 
         // Nessas 2 linhas deletamos a imagem  da sala
-        $caminho = "img-salas-santos/$codigo_sala";
-        unlink($caminho."/".$nome_img[0]);
+        $caminho = "img-salas-sao-paulo/$sala[0]";
+        unlink($caminho."/".$sala[1]);
         rmdir($caminho);
         header("Location: ../salas/santos.php");
-    }
-
-    $con->close();
 } else {
-    // Busca a senha da sala no banco de dados
-    $comandoSQL = "SELECT cd_senha_sala from tb_sala_sao_paulo WHERE cd_sala_sao_paulo='$codigo_sala'";
-    $resultado_usuario = mysqli_query($con, $comandoSQL) or die("Erro no banco de dados!");
-    $senha = mysqli_fetch_array($resultado_usuario);
-
-    if ($senha_sala === $senha[0]) {
         // Pega o nome da imagem no banco de dados
-        $comandoSQL = "SELECT img_sala from tb_sala_sao_paulo WHERE cd_sala_sao_paulo='$codigo_sala'";
-        $resultado_sala = mysqli_query($con, $comandoSQL) or die("Erro no banco de dados!");
-        $nome_img = mysqli_fetch_array($resultado_sala);
-
-        // Deleta os usuários que possuem a chave estrangeira da sala
-        $comandoSQL = "DELETE FROM tb_usuario WHERE cd_sala_sao_paulo='$codigo_sala'";
-        $con->query($comandoSQL) or die("algo deu errado");
+        $comandoSQL = "SELECT  nm_sala, img_sala from tb_sala_sao_paulo WHERE cd_sala_sao_paulo='$codigo_sala'";
+        $resultado_sala = mysqli_query($con, $comandoSQL);
+        $sala = mysqli_fetch_array($resultado_sala);
 
         // Deleta a sala criada no banco de dados
         $comandoSQL = "DELETE FROM tb_sala_sao_paulo WHERE cd_sala_sao_paulo='$codigo_sala'";
         $con->query($comandoSQL) or die("algo deu errado");
+        $con->close();
 
         // Nessas 2 linhas deletamos a imagem  da sala
-        $caminho = "img-salas-sao-paulo/$codigo_sala";
-        unlink($caminho."/".$nome_img[0]);
+        $caminho = "img-salas-sao-paulo/$sala[0]";
+        unlink($caminho."/".$sala[1]);
         rmdir($caminho);
         header("Location: ../salas/saopaulo.php");
-    }
-    
-    $con->close();
 }
