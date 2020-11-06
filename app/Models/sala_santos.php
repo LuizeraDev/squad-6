f556d0cc4b134e2e19c4af94fe95091f81c6955b
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-05 23:58:31 -0300

    Reformulando banco de dados para nova versão

---
+++ app/Models/sala_santos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class sala_santos extends Model
+{
+    use HasFactory;
+}
--- database/migrations/2020_11_06_023405_users_table.php
+++ database/migrations/2020_11_06_023405_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class UsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -22,7 +22,13 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
+            $table->integer('cd_convidado_por')->nullable();
+            $table->integer('cd_fila_usuario')->nullable();
+            $table->integer('cd_sala_santos')->unsigned()->nullable();
+            $table->integer('cd_sala_sao_paulo')->unsigned()->nullable();
             $table->timestamps();
+            $table->foreign('cd_sala_santos')->references('cd_sala_santos')->on('tb_sala_santos')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('cd_sala_sao_paulo')->references('cd_sala_sao_paulo')->on('tb_sala_sao_paulo')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
