1865133439a285e86da751d165f0243e9e2a43f0
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-07 19:00:06 -0300

    Exibindo foto dos usuários na fila

--- resources/views/salas/filaSala.blade.php
+++ resources/views/salas/filaSala.blade.php
@@ -1,3 +1,6 @@
+<?php 
+$url='http://localhost:8080/squad-6/storage/app/public/';
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -16,21 +19,33 @@
         <!-- No php, pegamos dados através do campo "name" dos inputs -->
         <section class="container" align="center">
             <h2>Logotipo - Fifo</h2>
+
             @foreach($dadosUsuario as $dados)
             <p>E aew <b>{{$dados->name}}</b></p>
             <p>Você está na fila da sala <b>{{$nmSala}}</b> e sua posição é <b>{{$dados->cd_fila_usuario}}</b></p>
             @endforeach
+            
             <hr style="width: 30%;">
             <br>
+
             @if ($_SESSION['santos'])
                 @foreach ($filaSantos as $fila) 
-                    <p>Posição na fila: <b>{{$fila->cd_fila_usuario}}</b> Nome: <b>{{$fila->name}}</b> </p>
+                    @if($fila->profile_photo_path)
+                        <span>Foto do usuário:</span>
+                        <img src={{$url.$fila->profile_photo_path}} width=30 alt="Foto do usuário">
+                    @endif
+                    <span>Posição na fila: <b>{{$fila->cd_fila_usuario}}</b> Nome: <b>{{$fila->name}}</b></span>
                 @endforeach
             @else
                 @foreach ($filaSaoPaulo as $fila) 
-                     <p>Posição na fila: <b>{{$fila->cd_fila_usuario}}</b> Nome: <b>{{$fila->name}}</b> </p>
+                    @if($fila->profile_photo_path)
+                        <span>Foto do usuário:</span>
+                        <img src={{$url.$fila->profile_photo_path}} alt="Foto do usuário">
+                    @endif
+                     <span>Posição na fila: <b>{{$fila->cd_fila_usuario}}</b> Nome: <b>{{$fila->name}}</b></span>
                 @endforeach
             @endif
+
             <br><br>
             <a href="#">Vou Jogar</a>
             <br><br>
