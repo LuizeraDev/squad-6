b4d609ccef120990722603e37e074223f1b1f8fd
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-10 03:10:20 -0300

    Adicionando atributo de report na table de usuarios

--- database/migrations/2020_11_06_023405_users_table.php
+++ database/migrations/2020_11_06_023405_users_table.php
@@ -20,6 +20,7 @@ class UsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('status')->nullable();
+            $table->integer('report')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
