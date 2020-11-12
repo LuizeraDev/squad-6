1e776789284deeb9a100b0142fb7cc8402c6be8e
Carlos-kaspa <carloslimatwg@gmail.com>
2020-11-12 16:13:09 -0300

    fix: faltou fechar aspas

--- resources/views/salas/salas.blade.php
+++ resources/views/salas/salas.blade.php
@@ -123,7 +123,7 @@ $url='http://localhost:8080/squad-6/storage/app/public/';
                                 "<button class='inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent"
                                 + "rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700"
                                 + "active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray"
-                                + "disabled:opacity-25 transition ease-in-out duration-150 ml-4' class='deleteButton' >
+                                + "disabled:opacity-25 transition ease-in-out duration-150 ml-4' class='deleteButton'>"
                                 + "<a href='salas/sala/" 
                                 + dadosSalas.sala[i].nm_sala 
                                 + "/excluir/" 
