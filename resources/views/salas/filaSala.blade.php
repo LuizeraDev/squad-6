f858a800921a660bda8a29fa89ab4ec7b9f12fe3
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-12 15:53:22 -0300

    Aprimorando código Ajax na exibição da fila :fire: :rocket:

--- resources/views/salas/filaSala.blade.php
+++ resources/views/salas/filaSala.blade.php
@@ -33,84 +33,87 @@ $_SESSION['cd_sala'] = $salaId;
 
             <br><br>
             <div id="conteudo3"></div>
-            <div id="conteudo4"></div>
+            <!-- <div id="conteudo4"></div> -->
             <a href="{{$salaId}}/desistente">Desistir</a>
 
         </section>
     </main>
 
     <!-- Este script é necessário para fazer a conexão assíncrona com o AJAX -->
-    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
     <script>
-        // Função responsável por atualizar as salas
-        function atualizarSalas() {
+        // Função responsável por atualizar as filas
+        function atualizarFila() {
+            $.ajax({ 
+                url: "{{ route('filaConteudo') }}",
+                dataType: "json",
+                cache: false,
+            }).done(function (dadosFila) {
+                // Descarta todas as informações anteriores para novas informações
+                $('#conteudo1').html("");
+                $('#conteudo2').html("");
+                $('#conteudo3').html("");
 
-            var conteudo_usuario = document.all['conteudo1'];
-            var conteudo_fila = document.all['conteudo2'];
-            var conteudo_vou_jogar = document.all['conteudo3'];
-            conteudo_vou_jogar.innerHTML = "";
-            conteudo_fila.innerHTML = "";
-            conteudo_usuario.innerHTML = "";
-
-            $.get("{{ route('filaConteudo') }}", function (dadosFila) {
-
-                // Usuário que entrou na fila
+                // Informações do usuário que entrou na sala
                 for (i = 0; i < dadosFila.dadosUsuario.length; i++) 
                 {
-                    conteudo_usuario.innerHTML +=
-                    "E aew <b>" + dadosFila.dadosUsuario[i].name+"</b><br><br>" +
-                    "Você está na fila da sala <b>" + dadosFila.dadosUsuario[i].nm_sala +"</b> " +
-                    "e sua posição é <b>" + dadosFila.dadosUsuario[i].cd_fila_usuario;
-                    if(dadosFila.dadosFila[i + 1].report){
-                        alert("Você foi reportado!!");
-                    }
-                    
+                    $('#conteudo1').append(
+                        "E aew <b>" + dadosFila.dadosUsuario[i].name+"</b><br><br>" +
+                        "Você está na fila da sala <b>" + dadosFila.dadosUsuario[i].nm_sala +"</b> " +
+                        "e sua posição é <b>" + dadosFila.dadosUsuario[i].cd_fila_usuario
+                    );
                 }
-                
-                if (dadosFila.dadosUsuario[0].cd_fila_usuario == 1) {
-                    conteudo_vou_jogar.innerHTML = "<a href='{{$salaId}}/voujogar'>Vou Jogar</a><br><br>";
-                }
-                
+
                 // Todos os usuários
                 for (i = 0; i < dadosFila.dadosFila.length; i++) 
                 {
-                    // Verifica se o usuário tem um caminho de imagem
+                    // Verifica se o usuário tem um caminho de imagem, se ele tiver a imagem é exibida.
                     if (dadosFila.dadosFila[i].profile_photo_path) {
-                        conteudo_fila.innerHTML += "<img src='{{ $url }}" + dadosFila.dadosFila[i].profile_photo_path+"' width='40'> ";
+                        $('#conteudo2').append(
+                            "<img src='{{ $url }}" + dadosFila.dadosFila[i].profile_photo_path+"' width='40'> &nbsp;"
+                        );
                     }
 
-                    // Exibe o usuário
+                    // Exibe os usuários da fila da sala em questão
+                    $('#conteudo2').append(
+                        "Nome: <b>" + dadosFila.dadosFila[i].name+"</b> " +
+                        "Posição na fila: <b>" + dadosFila.dadosFila[i].cd_fila_usuario +"</b> "+ 
+                        "Status usuário: <b>" + dadosFila.dadosFila[i].status + "</b> "+
+                        "<a href='#"+i+"' onclick=reportar(this.href)> Reportar</a><br><br>"
+                    );
+                }
 
-                    conteudo_fila.innerHTML += "Nome: <b>" + dadosFila.dadosFila[i].name+"</b> " +
-                    "Posição na fila: <b>" + dadosFila.dadosFila[i].cd_fila_usuario +"</b> "+ 
-                    "Status usuário: <b>" + dadosFila.dadosFila[i].status + "</b><a href='#"+i+"' onclick=reportar(this.href)> Reportar</a><br><br>";
-
+                // Exibe o botão vou jogar se a posição da fila do usuário for igual a 1
+                if (dadosFila.dadosUsuario[0].cd_fila_usuario == 1) {
+                    $('#conteudo3').html("<a href='{{$salaId}}/voujogar'>Vou Jogar</a><br><br>");
                 }
 
 
-            }), 'JSON';
-
+                setTimeout("atualizarFila()", 3000) // 3 segundos / Tempo de espera de atualização dos dados
+            })
         }
+
         function reportar(pos){
-               $.get("{{ route('filaConteudo') }}", function (dadosFila){
+            $.ajax({ 
+                url: "{{ route('filaConteudo') }}", 
+                dataType: "json",
+                cache: false,
+            }).done(function (dadosFila){
                 // Pegar posição pelo href
                 tamanho = pos.substring(pos.indexOf("#") +1 );
                 id = dadosFila.dadosFila[tamanho].id;
                 url = this.locate
                 alert("Você está reportando " + dadosFila.dadosFila[tamanho].id);
-                window.location.href = "/reportar/"+ url + "/"+id;
+                //window.location.href = "/reportar/"+ url + "/"+id;
 
-            }), 'JSON';
+            });
         }
-
-        // Definindo intervalo que a função será chamada no caso 10 em 10 segundos
-        setInterval("atualizarSalas()", 10000);
 
         // Quando carregar a página
         $(function () {
             // Faz a primeira atualização
-            atualizarSalas();
+            atualizarFila();
         });
     </script>
 
