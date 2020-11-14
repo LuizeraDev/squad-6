b3c751ea3e0ad0127384222d7fbc9d88fd84c2f8
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-14 01:51:47 -0300

    Exibindo pessoas que estão "jogando" juntamente com o usuário

--- resources/views/salas/utilizandoSala.blade.php
+++ resources/views/salas/utilizandoSala.blade.php
@@ -1,3 +1,6 @@
+<?php 
+$url='http://localhost:8080/squad-6/storage/app/public/';
+?>
 <!DOCTYPE html>
 
 <html lang="pt-br">
@@ -18,12 +21,57 @@
 <body>
     <main>
         <section class="container" align=center>
-            <h2>Selecione um botão após terminar</h2>
+            <h2>Utilizando a Sala</h2>
+            <div id="conteudo"></div>
+            <br><br>
+            <h3>Selecione um botão após terminar</h3>
             <a href="Voltar">Voltar para o final da fila</a>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="Acabei">Sair da sala</a>
         </section>
     </main>
+
+    <!-- Este script é necessário para fazer a conexão assíncrona com o AJAX -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+    <script>
+        function atualizarFila() {
+                
+                $.ajax({ 
+                    url: "{{ route('filaConteudo') }}",
+                    dataType: "json",
+                    cache: false,
+                }).done(function (dadosFila) {
+
+                    // Descarta todas as informações anteriores para novas informações
+                    $('#conteudo').html("");
+                    
+
+                    for (i = 0; i < dadosFila.Utilizando.length; i++) 
+                    {
+                        // Exibe foto do usuário se existir
+                        if (dadosFila.Utilizando[i].profile_photo_path) {
+                            $('#conteudo').append(
+                                "<img src='{{ $url }}" + dadosFila.Utilizando[i].profile_photo_path+"' width='40'> &nbsp;"
+                            );
+                        }
+
+                        // Exibe usuários que estão utilizando a sala
+                        $('#conteudo').append(
+                            "Nome: <b>" + dadosFila.Utilizando[i].name+"</b>&nbsp;&nbsp;" +
+                            "Status: <b>Em andamento</b>"
+                        );
+                    }
+                
+                    setTimeout("atualizarFila()", 3000) // 3 segundos / Tempo de espera de atualização dos dados
+                })
+            }
+            // Quando carregar a página
+            $(function () {
+                // Faz a primeira atualização
+                atualizarFila();
+            });
+    </script>
 </body>
 
 </html>
