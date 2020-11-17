0e91168d8c1781c61f2319af1de9a65fc0fca9b6
Carlos-kaspa <carloslimatwg@gmail.com>
2020-11-17 15:37:55 -0300

    ajuste na responsividade entre dispositivos médios e grandes

--- resources/views/salas/salas.blade.php
+++ resources/views/salas/salas.blade.php
@@ -1,19 +1,15 @@
 <?php 
 session_start();
 $url='http://localhost:8080/squad-6/storage/app/public/';
-
 // Verifica se o usuário já entrou em alguma sala
 if (isset($_SESSION['entrou_sala']) && isset($_SESSION['cd_sala']) && $_SESSION['entrou_sala']) {
-
     // pega o Id da sala que ele deixou pelo botão voltar do browser
     $id_sala_anterior = $_SESSION['cd_sala']; 
-
     /* 
     * Resolvendo problema do usuário quando ele tenta voltar pelo browser
     * Basicamente eu passo o parâmetro com a sala anterior dele e retiro ele da sala 
     * mandando ele pro controler de desistência da fila
     */
-
    echo "<script>window.location.href='/salas/sala/nomesala/". $id_sala_anterior ."/desistente'</script>";
 }
 ?>
@@ -67,7 +63,7 @@ if (isset($_SESSION['entrou_sala']) && isset($_SESSION['cd_sala']) && $_SESSION[
                 </div>
 </header>
 
-<div class="grid min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0"> <!--- GRID -->
+<div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0"> <!--- GRID -->
 
     <section class="salasContainer">
          
@@ -82,58 +78,43 @@ if (isset($_SESSION['entrou_sala']) && isset($_SESSION['cd_sala']) && $_SESSION[
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
     <script>
-
         // Função responsável por atualizar as salas
         function atualizarSalas() {
             //cria container para inserir as salas
-            var conteudo_salas = $("<div/>").addClass("conteudoContainer").addClass("sm:flex mb-4").appendTo("section");
-
+            var conteudo_salas = $("<div/>").addClass("conteudoContainer").addClass("grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-4").appendTo("section");
             conteudo_salas.innerHTML = "";
             //cria array das salas
             var wrapperSala = []
-
             var newSala = 
             $("<div/>")
             .addClass("criarSala")
-            .addClass("max-w-sm rounded overflow-hidden shadow-lg bg-gray-200 tracking-widest")
-            .addClass("bg-gray-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-gray-200 rounded");
-
+            .addClass("max-w-sm md:w-1/2 lg:w-1/3 rounded overflow-hidden shadow-lg bg-gray-200 tracking-widest")
+            .addClass("bg-gray-500 hover:bg-blue-600 text-white font-bold border border-gray-200 rounded");
               
             newSala.append( "<a href='criarsala'>Criar Sala</a>"  )
-
-
                 $.get("{{ route('salasConteudo') }}", function (dadosSalas) {
-
                     $contador = 0; // contador de usuários para a sala
-
                     // Exibe informações sobre os usuários cadastrados / online / ausente / offline, sala em que está
                     for (sala of dadosSalas.usuarios){
-
                             console.log(dadosSalas.usuarios[sala] ); 
                         }
                 
                     for (i = 0; i < dadosSalas.sala.length; i++){   
                         //cria sala individual baseada nas salas do DB
-                        wrapperSala[i] = $("<div/>").addClass("wrapper").addClass("max-w-sm rounded overflow-hidden shadow-lg bg-gray-200");
-
+                        wrapperSala[i] = $("<div/>").addClass("wrapper").addClass("max-w-sm lg:w-1/3 md:1/2 rounded overflow-hidden shadow-lg bg-gray-200");
                         wrapperSala[i].append(
                             "<img class='w-full' src='{{ $url }}" + dadosSalas.sala[i].img_sala 
                             + "' width=200>"
                         );
-
                         wrapperSala[i].append("<div class='px-6 py-4'>");
-
                         wrapperSala[i].append("<h3 class='font-bold text-xl mb-2'><b>" + dadosSalas.sala[i].nm_sala + "</b></h3>");
-
                         // Conta a quantidade de pessoas em uma determinada sala.
                         for(c = 0; c < dadosSalas.qt_usuarios.length; c++){
                             // Faz a verificação se o nome da sala é o mesmo do usuário.
                             if (dadosSalas.sala[i].nm_sala == dadosSalas.qt_usuarios[c].nm_sala) {
-
                                 $contador *= 1 ; //adiciona +1 para cada usuário na                                
                             } 
                         }
-
                         wrapperSala[i].append(
                             "<p> Usuários na sala: <b>"
                             + $contador 
@@ -144,9 +125,9 @@ if (isset($_SESSION['entrou_sala']) && isset($_SESSION['cd_sala']) && $_SESSION[
                         
                         wrapperSala[i].append(
                             //classes do tailwind css
-                            "<button class='inline-flex items-center px-4 py-2 bg-blue-600 border" 
+                            "<button class='enterButton inline-flex items-center  bg-gray-600 border" 
                             + "border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest"
-                    
+                            + "hover:bg-blue-600"
                             //fim tailwind
                             + "class='enterButton'>"//adiciona classe
                             //rota da sala
@@ -157,15 +138,12 @@ if (isset($_SESSION['entrou_sala']) && isset($_SESSION['cd_sala']) && $_SESSION[
                                 } else { 
                                     echo "dadosSalas.sala[i].cd_sala_sao_paulo"; 
                                     } ?> 
-
                             + "'>Entrar na Sala</a></button>"
                         );
-
                             wrapperSala[i].append(
-                                "<button class='inline-flex items-center px-4 py-2 bg-orange-300 border" 
+                                "<button class='deleteButton inline-flex items-center bg-gray-400 border" 
                                 + "border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest"
-                                + " ml-4'"
-                                //fim tailwind
+                                + "hover:bg-blue-600"                                //fim tailwind
                                 + "class='deleteButton'>" //adiciona classe
                                 //rota da sala
                                 + "<a href='salas/sala/" + dadosSalas.sala[i].nm_sala + "/excluir/" + dadosSalas.sala[i].cd_sala_santos 
@@ -173,17 +151,14 @@ if (isset($_SESSION['entrou_sala']) && isset($_SESSION['cd_sala']) && $_SESSION[
                             );                 
                         
                     }
-
                     
                     conteudo_salas.append(wrapperSala);
                   
-
                     conteudo_salas.append(newSala)
                 })
                 
                 
         }
-
         // Definindo intervalo que a função será chamada no caso 10 em 10 segundos
         
         // Quando carregar a página
