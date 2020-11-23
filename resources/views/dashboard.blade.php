b13163c9dce6d8909a8f70648a9eaed72707b9d7
Carlos-kaspa <carloslimatwg@gmail.com>
2020-11-23 20:44:47 -0300

    identidade visual tela Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,10 @@ $_SESSION['dashboard'] = true;
 ?>
 
 <x-app-layout>
-<div class="flex flex-col sm:justify-center items-center mb-4">
 
+<div class="background w-full" alt=""></div>
+
+<div class="body flex flex-col sm:justify-center items-center mb-4">
     <div class="greetings sm:justify-center items-center">
         <h3>Bem-vindo ao FIFO</h3>
         <p>Selecione a unidade que você está</p>
@@ -18,8 +20,8 @@ $_SESSION['dashboard'] = true;
         <a class="option max-w-sm md:w-1/2  rounded overflow-hidden shadow-lg tracking-widest
                 bg-purple-600 hover:bg-purple-700 text-white font-bold border-none text-2xl rounded" href="/unidade/saopaulo">São Paulo</a>
     </div>
-    
-</div>
+</div>   
+
   
 
 </x-app-layout>
