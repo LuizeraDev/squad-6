6299d49932176820633a28b297eaae3efef80c97
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-11-24 19:10:48 -0300

    Tela de unidade e login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,15 +9,16 @@ $_SESSION['dashboard'] = true;
 
 <div class="body flex flex-col sm:justify-center items-center mb-4">
     <div class="greetings sm:justify-center items-center">
-        <h3>Bem-vindo ao FIFO</h3>
-        <p>Selecione a unidade que você está</p>
+        <p>Veja a fila das atividades que estão rolando no escritório.</p>
+
+        <h3>Nos diga, onde você está agora?</h3>
     </div>
 
     <div class="grid sm:grid-cols-1 md:grid-cols-2 sm:justify-center items-center mb-4">
         <a class="option max-w-sm md:w-1/2 rounded overflow-hidden shadow-lg tracking-widest
                     bg-purple-600 hover:bg-purple-700 text-white font-bold border-none text-2xl rounded" href="/unidade/santos">Santos</a>
         
-        <a class="option max-w-sm md:w-1/2  rounded overflow-hidden shadow-lg tracking-widest
+        <a class="option max-w-sm md:w-1/4  rounded overflow-hidden shadow-lg tracking-widest
                 bg-purple-600 hover:bg-purple-700 text-white font-bold border-none text-2xl rounded" href="/unidade/saopaulo">São Paulo</a>
     </div>
 </div>   
