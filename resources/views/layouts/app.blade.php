b714b6a20cc43a18a9e3bfa99807945d0a54f974
Carlos-kaspa <carloslimatwg@gmail.com>
2020-11-26 14:58:38 -0300

    remoção tailwindCSS, adição media query

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Importando tailwind -->
@@ -19,15 +18,15 @@
         @livewireStyles
     </head>
 
-    <body class="font-sans antialiased">
-        <div class="min-h-screen">
+    <body>
+        <div class="navBar">
             @livewire('navigation-dropdown')
             
             <!-- Page Heading -->
         
 
             <!-- Page Content -->
-            <main>
+            <main class="main">
                 {{ $slot }}
             </main>
             
--- resources/views/salas/unidade.blade.php
+++ resources/views/salas/unidade.blade.php
@@ -5,9 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fifo - Squad 6</title>
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.6/tailwind.min.css" 
-    integrity="sha512-l7qZAq1JcXdHei6h2z8h8sMe3NbMrmowhOl+QkP3UhifPpCW2MC4M0i26Y8wYpbz1xD9t61MLT9L1N773dzlOA==" crossorigin="anonymous" />
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -24,20 +21,21 @@
 <body>
 @livewire('navigation-dropdown')
 
-
-    <div class="min-h-full flex sm:justify-center items-center md:pt-0 lg:pt-0 sm:pt-0 xl:pt-0 mt-10"> <!--- GRID -->
         <!-- No php, pegamos dados através do campo "name" dos inputs -->
-        
-        <section class="flex flex-col sm:justify-center items-center w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"> <!-- box igual do login -->
             <h2>Logotipo - Fifo</h2>
             <p>Escolha a unidade em que você está</p>
-            <a class="option max-w-sm overflow-hidden shadow-lg tracking-widest bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 border border-none rounded" href="/unidade/santos">Santos</a>
+
+        <section class="grid"> <!-- box igual do login -->
+           
+            <a class="option " href="/unidade/santos">Santos</a>
             
-            <a class="option max-w-sm overflow-hidden shadow-lg tracking-widest bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 border border-none rounded" href="/unidade/saopaulo">São Paulo</a>
+            <a class="option " href="/unidade/saopaulo">São Paulo</a>
             
-            <a href="dashboard">Voltar</a>
+            
         </section>
-    </div>
+
+        <a href="dashboard">Voltar</a>
+  
 </body>
 
 </html>
